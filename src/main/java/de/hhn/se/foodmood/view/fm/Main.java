bea4e26343cd9b8c4148437e4aa5d543310f7d1b
Bastipreme <dieserbasti03@gmail.com>
2023-12-18 10:22:26 +0100

    ssss

--- src/main/java/de/hhn/se/foodmood/view/fm/Main.java
+++ src/main/java/de/hhn/se/foodmood/view/fm/Main.java
@@ -2,6 +2,6 @@ package de.hhn.se.foodmood.view.fm;
 
 public class Main {
   public static void main(String[] args) {
-    de.hhn.se.pmt.foodmood.view.foodmood.MainPMT.main(args);
+    MainPMT.main(args);
   }
 }
--- src/main/java/de/hhn/se/foodmood/view/fm/MainPMT.java
+++ src/main/java/de/hhn/se/foodmood/view/fm/MainPMT.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainPMT extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(MainPMT.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(MainPMT.class.getResource("home.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 688, 459);
     stage.setTitle("PMT");
     stage.setScene(scene);
