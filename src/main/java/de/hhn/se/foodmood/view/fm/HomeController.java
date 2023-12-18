a2f04070feca0aafaad2e8936a0cdf1eb92a1a4f
Bastipreme <dieserbasti03@gmail.com>
2023-12-18 13:32:28 +0100

    ssss

--- src/main/java/de/hhn/se/foodmood/view/fm/HomeController.java
+++ src/main/java/de/hhn/se/foodmood/view/fm/HomeController.java
@@ -2,7 +2,8 @@ package de.hhn.se.foodmood.view.fm;
 
   import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+  import javafx.fxml.FXMLLoader;
+  import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
   public class HomeController {
@@ -15,6 +16,7 @@ import javafx.scene.image.ImageView;
 
     @FXML
     void createButton(ActionEvent event) {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("create.fxml"));
 
     }
 
